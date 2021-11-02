c97b717780652265e4be1ee7ddf6e876a7240523
John Merz <jmerz826@gmail.com>
2021-11-02 18:01:34 -0400

    posts are displaying incorrectly, but displaying nonetheless

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar setSearchBar={setSearchBar} />
-      <Posts setPosts={setPosts} />
+      <Posts likePost={likePost} posts={posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
